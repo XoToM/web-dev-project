d7bdd346bcfbda59510c4e5fb75e6108b1609d20
XoToM <xotom.11.23@gmail.com>
2023-04-21 00:21:05 +0100

    basic world generation function

--- mapManager.js
+++ mapManager.js
@@ -4,6 +4,59 @@ const VOXELMAP_WORLD_SIZE = [64, 64, 64];
 
 //	Use to transfer data to gpu ->	gl.texSubImage3D(gl.TEXTURE_3D, 0, ox, oy, oz, width, height, depth, gl.RGBA, gl.UNSIGNED_BYTE, data);
 
+class TmpBrick {
+	data = null;
+	constructor(){
+		this.data = new Uint8Array(8*8*8*4);
+		for(let i = 0; i < this.data.length; i++){
+			this.data[i] = 0;
+		}
+	}
+	setVoxelEmpty(lx, ly, lz){
+		let pointer = 8*8*lz + 8*ly + lx;
+		pointer = pointer*4;
+		data[pointer++] = 0;
+		data[pointer++] = 0;
+		data[pointer++] = 0;
+		data[pointer++] = 0;
+	}
+	setVoxelColor(lx, ly, lz, r, g, b){
+		let pointer = 8*8*lz + 8*ly + lx;
+		pointer = pointer*4;
+		data[pointer++] = r;
+		data[pointer++] = g;
+		data[pointer++] = b;
+		data[pointer++] = 1;
+	}
+	setVoxelPointer(lx, ly, lz, brickPos){
+		let pointer = 8*8*lz + 8*ly + lx;
+		pointer = pointer*4;
+		data[pointer++] = brickPos[0];
+		data[pointer++] = brickPos[1];
+		data[pointer++] = brickPos[2];
+		data[pointer++] = 2;
+	}
+	setVoxelRaw(lx, ly, lz, data){
+		let pointer = 8*8*lz + 8*ly + lx;
+		pointer = pointer*4;
+		data[pointer++] = data[0];
+		data[pointer++] = data[1];
+		data[pointer++] = data[2];
+		data[pointer++] = data[3];
+	}
+	checkIsAllEmpty(){
+		for(let i=0;i<8*8*8;i++){
+			if(this.data[4*i+3] != 0) return false;
+		}
+		return true;
+	}
+	checkIsAllSolid(){
+		for(let i=0;i<8*8*8;i++){
+			if(this.data[4*i+3] != 1) return false;
+		}
+		return true;
+	}
+}
 
 class VoxelMap {
 	voxelMap = null;
@@ -35,4 +88,16 @@ class VoxelMap {
 
 	}
 
+}
+function generationFunction(x, y, z){		//	Basic world generation function to create an rgb sphere out of voxels
+	let tx = x-VOXELMAP_WORLD_SIZE[0]/2;
+	let ty = y-VOXELMAP_WORLD_SIZE[1]/2;
+	let tz = z-VOXELMAP_WORLD_SIZE[2]/2;
+
+	if(tx*tx+ty*ty+tz*tz>5)return [0,0,0,0];
+
+	let r = Math.floor(x / VOXELMAP_WORLD_SIZE[0] * 256);
+	let g = Math.floor(y / VOXELMAP_WORLD_SIZE[1] * 256);
+	let b = Math.floor(z / VOXELMAP_WORLD_SIZE[2] * 256);
+	return [r,g,b,1]
 }
