b253492723894a797432d591d5a28d6b567b55d7
XoToM <xotom.11.23@gmail.com>
2023-05-28 18:12:26 +0100

    Added example files

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let hex;
 let current_drag_object = null;
 const drag_overlay = document.getElementById("drag-overlay");
+const example_selector = document.getElementById("example-selection");
 
 {		//	Initialization code which makes the hex viewer display increasing bytes
 	let data = [];	//	Generate bytes
@@ -9,6 +10,7 @@ const drag_overlay = document.getElementById("drag-overlay");
 	}
 
 	hex = loadHex("memory", data);	//	Initialize the hex viewer with data
+	example_selector.selectedIndex = 1;	//	Some browsers like to save settings between reloads, but this sample data gets loaded immediately. This means that I have to reset the selected option in the dropdown manualy every time the page loads
 }
 
 function onFileHover(event){	//	Prevent the browser from messing with our dragging behaviour
@@ -29,24 +31,51 @@ function onDragEnd(event){
 	drag_overlay.style.zIndex = -100;
 }
 
-async function onFileInputChange(event){
+async function onFileInputChange(event){	//	On File loaded through button
 	if(event.target.files && event.target.files.length){	//	Check if there are any files to be loaded
 		let file = event.target.files[0];	//	Get the first file
 		let ab = await file.arrayBuffer();
+		example_selector.selectedIndex = 0;
 		await loadBuffer(ab);				//	Load the file
+	}
+}
+async function onExampleSelected(event){
+	switch(event.target.selectedIndex){	//	Do a different thing depending on what is selected in the dropdown
+		case 1:
+			hex.length = 0;
+			for(let i=0;i<256;i++){
+				hex.push(i%256);
+			}
+			break;
+		case 2:
+			await loadHexFile("./examples/test.nbt");
+			break;
+		case 3:
+			await loadHexFile("./index.html");
+			break;
+		case 4:
+			await loadHexFile("./styles.css");
+			break;
+		case 5:
+			await loadHexFile("./main.js");
+			break;
+		case 6:
+			await loadHexFile("./hexedit.js");
+			break;
 	}
 }
 
 //	Fetch and load file from path
 async function loadHexFile(path){
 	let response = await fetch(path);
-	let buffer = await response.arrayBuffer;
+	let buffer = await response.arrayBuffer();
 	await loadBuffer(buffer);
 }
 
 //	Load dropped file
 async function loadFile(event){
 	event.preventDefault();		//	Prevent the browser from opening the file in this browser tab. This would close the page which we dont want
+	example_selector.selectedIndex = 0;
 
 	if(event.dataTransfer.files && event.dataTransfer.files.length){	//	Check if there are any files to be loaded
 		let file = event.dataTransfer.files[0];	//	Get the first file
