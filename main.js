ed2b073e8b42897dc319cce4323401a250b52714
XoToM <xotom.11.23@gmail.com>
2023-05-28 14:07:47 +0100

    Minor improvements, file drop button, comments

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 let hex;
+let current_drag_object = null;
+const drag_overlay = document.getElementById("drag-overlay");
 
-{
-	let data = [];
-	for(let i=0;i<2560;i++){
+{		//	Initialization code which makes the hex viewer display increasing bytes
+	let data = [];	//	Generate bytes
+	for(let i=0;i<256;i++){
 		data.push(i%256);
 	}
-	hex = loadHex("memory", data, 4);
+
+	hex = loadHex("memory", data, 4);	//	Initialize the hex viewer with data
 }
 
-function onFileHover(event){
+function onFileHover(event){	//	Prevent the browser from messing with our dragging behaviour
 	event.preventDefault();
 }
+
 function onDragStart(event){
-	if(event.dataTransfer.items.length && event.dataTransfer.items[0].kind === "file"){
-		document.getElementById("drag_overlay").style.opacity = 100;
-		document.getElementById("drag_overlay").style.zIndex = 100;
+	if(event.dataTransfer.items.length && event.dataTransfer.items[0].kind === "file"){	//	Only handle this event if the user is dragging a file
+		current_drag_object = event.target;
+
+		drag_overlay.style.opacity = 100;	//	Show the file drop overlay
+		drag_overlay.style.zIndex = 100;
 	}
 }
 function onDragEnd(event){
-	document.getElementById("drag_overlay").style.opacity = 0;
-	document.getElementById("drag_overlay").style.zIndex = -100;
+	if(event && current_drag_object !== event.target) return;	//	Check if the element the user is hovering over has changed or if the user left the page. Ignore the event if the user is simply hovering over a different element
+	drag_overlay.style.opacity = 0;	//	Hide the file drop overlay
+	drag_overlay.style.zIndex = -100;
+}
+
+async function onFileInputChange(event){
+	if(event.target.files && event.target.files.length){	//	Check if there are any files to be loaded
+		let file = event.target.files[0];	//	Get the first file
+		let ab = await file.arrayBuffer();
+		await loadBuffer(ab);				//	Load the file
+	}
 }
 
 //	Fetch and load file from path
@@ -31,25 +46,22 @@ async function loadHexFile(path){
 
 //	Load dropped file
 async function loadFile(event){
-	console.log("File(s) dropped");
-	event.preventDefault();
+	event.preventDefault();		//	Prevent the browser from opening the file in this browser tab. This would close the page which we dont want
 
-	if(event.dataTransfer.files && event.dataTransfer.files.length){
-		let file = event.dataTransfer.files[0];
+	if(event.dataTransfer.files && event.dataTransfer.files.length){	//	Check if there are any files to be loaded
+		let file = event.dataTransfer.files[0];	//	Get the first file
 		let ab = await file.arrayBuffer();
-		await loadBuffer(ab);
-		//
-
-		//hex = loadHex("memory", data, 4);
+		await loadBuffer(ab);				//	Load the file
 	}
 }
 
 //Load file from buffer
 async function loadBuffer(data){
-	hex.length = 0;
-	data = new Uint8Array(data);
+	data = new Uint8Array(data);	//	Get the raw bytes
 
-	for(let i=0; i<data.length; i++){
-		hex.push((+data[i])&0b11111111);
+	hex.length = 0;	//	Clear all bytes in the hex viewer
+
+	for(let i=0; i<data.length; i++){	//	Load all bytes into the hex viewer
+		hex.push(data[i]);
 	}
 }
